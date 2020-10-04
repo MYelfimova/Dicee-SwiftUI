44c449c5c908b8b9be6d1ce7d2d20c80c2d0cdb3
MariaYelfimova <maria.yelfimova@gmail.com>
2020-10-04 09:54:43 +0300

    simple throw dice app, built on SwiftUI

--- Dicee-SwiftUI/Dicee_SwiftUIApp.swift
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -15,3 +15,9 @@ struct Dicee_SwiftUIApp: App {
         }
     }
 }
+
+struct Dicee_SwiftUIApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
